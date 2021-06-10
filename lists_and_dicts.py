f4bbeda0afe54ade479ca42fe3d72c2846a5c8f1
daynoryamil <dybautistaconde@gmail.com>
2021-06-10 14:07:02 -0400

    1 lists and dicts

---
+++ lists_and_dicts.py
@@ -0,0 +1,23 @@
+def run():
+    my_list = [1, "Hello", True, 4.5]
+    my_dict = {"firstname":"Daynor", "lastname":"Bautista"}
+
+    super_list = [
+        {"firstname":"Daynor", "lastname":"Bautista"},
+        {"firstname":"Pame", "lastname":"Bsta"},
+        {"firstname":"Ana", "lastname":"Bauti"},
+        {"firstname":"Rosa", "lastname":"Bauti"},
+        {"firstname":"Gri", "lastname":"Bautist"},
+    ]
+
+    super_dict ={
+        "nautral_nums":[1,2,3,4,5],
+        "integer_nums":[-1,-2,0,1,2],
+        "floating_nums":[1.1,4.5,6.4]
+    }
+
+    for key, value in super_dict.items():
+        print(key, "-",value)
+
+if __name__ == "__main__":
+    run()
